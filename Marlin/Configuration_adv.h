c1b82cc25fa83f16b3dc13bee47a943a0532b2c6
Demetrius Robinson <djayer422@hotmail.com>
2020-09-07 12:33:49 -0400

    Modified E2_AUTO_FAN_PIN
    Changed value of E2_AUTO_FAN_PIN from PS_ON_PIN to -1

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -454,7 +454,7 @@
  */
 #define E0_AUTO_FAN_PIN FAN1_PIN
 #define E1_AUTO_FAN_PIN FAN1_PIN
-#define E2_AUTO_FAN_PIN PS_ON_PIN
+#define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
 #define E4_AUTO_FAN_PIN -1
 #define E5_AUTO_FAN_PIN -1
